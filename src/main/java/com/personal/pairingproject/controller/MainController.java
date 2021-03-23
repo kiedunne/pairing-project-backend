858a5114e963cdf86d5a20091cf056bd4790a750
Jackie Dunne <kie.dunne@gmail.com>
2021-03-23 20:54:19 +0000

    Rest controller and cross origin

--- src/main/java/com/personal/pairingproject/controller/MainController.java
+++ src/main/java/com/personal/pairingproject/controller/MainController.java
@@ -1,13 +1,18 @@
 package com.personal.pairingproject.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class MainController {
 
+    @CrossOrigin("*")
     @GetMapping(value = "health-check")
     public String healthCheck() {
+        log.info("health check hit");
         return "OK";
     }
 }
